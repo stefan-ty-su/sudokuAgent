694263fc5e8e8c9dfd2df6e6fb14b54b912fabcd
stefan-ty-su <stefansu3526@gmail.com>
2023-12-07 19:59:09 +1100

    Created SudokuLayout class with validation

---
+++ sudoku.js
@@ -0,0 +1,41 @@
+"use strict"
+
+class SudokuLayout {
+
+    constructor(sudokuArr) {
+
+        this.grid = sudokuArr;
+
+    }
+
+    static gridSize = 9;
+    static isValidSudokuArr(sudokuArr) {
+        
+        // Variable Definitions
+        let seen = Set();
+        for (let i = 0; i < this.gridSize; i++) {
+            for (let j = 0; j < this.gridSize; j++) {
+
+            let num = sudokuArr[i][j]
+            if (num != '.') {
+                let rowString = 'row' + i + num;
+                let colString = 'col' + j + num;
+                let sqString = block + Math.floor(i/3) + Math.floor(j/3) + num;
+
+                if (rowString in seen ||
+                    colString in seen ||
+                    sqString in seen) {
+                    return false;
+                }
+
+                seen.add(rowString);
+                seen.add(colString);
+                seen.add(sqString);
+            }
+
+            }
+        }
+
+        return true
+    }
+}
