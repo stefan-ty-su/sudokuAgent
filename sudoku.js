05d7a3b17a61e333ac501bc80e67cb84463fb660
stefan-ty-su <stefansu3526@gmail.com>
2023-12-07 23:27:23 +1100

    Added method to check if layout is complete

--- sudoku.js
+++ sudoku.js
@@ -162,6 +162,10 @@ class SudokuLayout {
 
         return null;
     }
+    
+    isComplete() {
+        return this.seen.size == 243;
+    }
 }
 
 let sa = [
@@ -175,8 +179,20 @@ let sa = [
     [".",".",".","4","1","9",".",".","5"],
     [".",".",".",".","8",".",".","7","9"]
 ];
+sa = [ // Valid Solved Sudoku
+    ["1","2","3","6","7","8","9","4","5"],
+    ["5","8","4","2","3","9","7","6","1"],
+    ["9","6","7","1","4","5","3","2","8"],
+    ["3","7","2","4","6","1","5","8","9"],
+    ["6","9","1","5","8","3","2","7","4"],
+    ["4","5","8","7","9","2","6","1","3"],
+    ["8","3","6","9","2","4","1","5","7"],
+    ["2","1","9","8","5","7","4","3","6"],
+    ["7","4","5","3","1","6","8","9","2"]
+];
 
 let layout = new SudokuLayout(sa);
+// console.log(layout.validMoves);
+// layout.playMove(8, 0, 1)
 console.log(layout.validMoves);
-layout.playMove(8, 0, 1)
-console.log(layout.validMoves);
+console.log(layout.isComplete());
